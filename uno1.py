75174ff6941bb70ffae33ea07b02957014aa9edb
Marca <bainmarca@yahoo.com>
2018-11-25 08:37:40 -0600

    fixed notes

--- uno1.py
+++ uno1.py
@@ -28,7 +28,7 @@ for i in range(0,4):
 #shuffle deck
 random.shuffle(deck)   
 # create player hands
-#put cards in 3 different hands, this is easy to change how many players
+#put cards in 5 different hands, this is easy to change how many players
 numplayers=5
 hand=[]
 #initalize hand list to empty hand
@@ -82,36 +82,32 @@ clockwise=True
 i=0   #start with human player
 wildcolor=''  #empty because it gets filled in when one is played by human or com.
 
-#while it is the start of the game the card is a wild draw4
-# we will need to append the card back to the deck, then delet it
-# from the discard, and reshuffle
-#we use a while loop, because it could happen again
+#if 1st card is a draw4, we put back into deck and shuffle to restart
 while discard[-1]=={'color':'black','face':'draw4'}:
     deck.append(discard[-1])
     del discard[-1]
     random.shuffle(deck)  
     discard.append(deck.pop())
-#if is for if the 1st card is just a wild to pick color
-# the first person can just choose a color and the game begins
+#if 1st card is a wild, player 1 chooses a color and begins game
 if discard[-1]=={'color':'black','face':'wild'}:
     printgamestate(discard,hand,numplayers)
     legalcolor=False
     while not legalcolor:
         wildcolor=input('choose a color: ')
         legalcolor=wildcolor in color 
-#if 1st card is a draw2, then first player draws to and game goes to 2nd player
+#if 1st card is a draw2, then player 1 draws to and game goes to player 2
 if discard[-1]['face']=='draw2':
     printgamestate(discard,hand,numplayers)
     hand[0].append(deck.pop())
     hand[0].append(deck.pop())
     print('player 1 drew 2 cards')
-    i=1
-#if 1st card is a skip
+    i=1    #player 2 plays
+#if 1st card is a skip, game will skip player 1, and go to player 2
 if discard[-1]['face']=='skip': 
     printgamestate(discard,hand,numplayers)
     print('player 1 got skipped')
     i=1
-#if 1st card is a reverse
+#if 1st card is a reverse, the game will go 1,3,2
 if discard[-1]['face']=='reverse':
     clockwise=False
  
@@ -120,10 +116,10 @@ drawcards=0         #set to 0, because a draw card hasn't been played yet
 gameover=False  #game is not over until we say it is, when someone is out of cards
 print ("Let's play Uno with " + str(numplayers) + " players, type 'quit' to exit game")
 while not gameover:
-    if len(deck)==0:
+    if len(deck)==0:    #119-126, if deck is empty, put discard back to deck and shuffle
         print ('deck is empty, we will shuffle discard into deck')
         topdiscard=discard[-1]
-        del discard[-1]
+        del discard[-1]     #we need to leave top card for discard pile
         deck=discard.copy()
         discard.clear()
         discard.append(topdiscard)
@@ -137,13 +133,11 @@ while not gameover:
                 cardtoplay=int(cardtoplay)  #cardtoplay my cards to choose from
                 if cardtoplay not in range(1,len(hand[i])+2):  #if number was wrong
                     print ('enter valid number, 1-' + str(len(hand[i])+1))
+#138-161 if I drew a card, and if it is playable it will say I drew a card and I played it
+#if I did not draw a playable card, it would go to next player
                 elif cardtoplay==len(hand[i])+1:  
                     hand[i].append(deck.pop())   #card I draw goes in to hand
                     cardlegal=True
-#129 is if I do not have a card to play I will have to draw a card, 138 prints card I drew
-#161 if card drawn is playable it automaticly gets played and is appened to discard
-#option since we list out the cards and then make that choice next
-# (so it is alwyas len(hand[i]+1))
                     print ("you drew: " + printcard(hand[i][-1]))
                     if canplay(discard[-1],hand[i][-1],wildcolor):
                         print ("you played it") #says whet card I drew I played
@@ -152,11 +146,7 @@ while not gameover:
                             while not legalcolor:
                                  wildcolor=input('choose a color: ')
                                  legalcolor=wildcolor in color  
-#line 144 the wildcolor is only the colors in the list color at the start of code
-#138-143 is only if I did not have a card to play, and the card that I drew
-#was a wild and that was played, below 107-110 is if I drew a non wild card that could
-#be played     
-#lines 150 to 160 is if the card drawn is not a wild, it just gets played
+#wildcolor is in the color list at the beginning
                         else:
                             wildcolor=''  #if there is no wild card played
                         if hand[i][-1]['face']=='draw2':
@@ -169,16 +159,14 @@ while not gameover:
                             clockwise=not clockwise
                         discard.append(hand[i][-1])  #card went on discard pile
                         hand[i].pop()  #card was poped out of hand
-#line 163 is if I did not have to draw a card, but played a wild card I had in hand                        
+#line 163-181 if I didn't have to draw a card, and played a card in my hand                        
                 elif canplay(discard[-1],hand[i][cardtoplay-1],wildcolor):
                     cardlegal=True  #goes to this step if card in hand can play
                     if hand[i][cardtoplay-1]['color']=='black':
                         legalcolor=False
                         while not legalcolor:
                             wildcolor=input('choose a color: ')
-                            legalcolor=wildcolor in color
-#line 168 how to choose a legal color, blue, yellow, red or green from list at beginning
-#lines 173-183 if I play a card that is not wild                          
+                            legalcolor=wildcolor in color                         
                     else:
                         wildcolor=''  #if there is no wild card played
                     if hand[i][cardtoplay-1]['face']=='draw2':
@@ -197,8 +185,8 @@ while not gameover:
                 if cardtoplay=="quit":
                     sys.exit()  #when we run the game we can stop it
                 print("invalid number")
-#lines 122 to 190 is for the human player              
-    else:
+#lines 127 to 183 is for the human player              
+    else:       # if computer is playing
         complayedcard=False
         j=0  #j is the card in the hand of com.player
         while j<len(hand[i]):  #we inerate through hand to find legal card to play
@@ -207,9 +195,10 @@ while not gameover:
                 if hand[i][j]['color']=='black':
                     wildcolor=color[random.randint(0,3)]
                     print ('complayer ' + str(i+1) + ' chose ' + wildcolor)
-#195-199 if com. plays a wild they choose blue
-#lines below ending at j+=1 is if computer did not play a wild
-# we have the code to inerate through the com had to find 1st legal card
+#line 194 prints what com. player plays, line 195 if they play a wild, they choose a random
+#color, from the list of colors at the beginning of code. we print that too, so
+#so we know what color to play
+#line 194-215 if computer plays a playable card
                 else:
                     wildcolor=''  #close out wild if one was not played
                 if hand[i][j]['face']=='draw2':
@@ -225,7 +214,7 @@ while not gameover:
                 complayedcard=True
                 break  #stop looking for legal cards
             j+=1  #inerate though cards in hand
-#lines 219 to 226 is if the com had to draw a card, and that card was a wild, and it got played
+#lines 218 to 237 is if com. player had to draw a card
         if not complayedcard:
             print ('complayer ' + str(i+1) + ' drew a card ')
             hand[i].append(deck.pop())
@@ -233,8 +222,7 @@ while not gameover:
                 print ('complayer ' + str(i+1) + ' played '+ printcard(hand[i][-1]))
                 if hand[i][-1]['color']=='black':
                     wildcolor=color[random.randint(0,3)]
-                    print ('complayer ' + str(i+1) + ' chose ' + wildcolor)            
-#lines 228 to 239 is if the com. drew a card that was not wild, but was playable                   
+                    print ('complayer ' + str(i+1) + ' chose ' + wildcolor)                              
                 else:
                     wildcolor=''
                 if hand[i][-1]['face']=='draw2':
@@ -250,9 +238,9 @@ while not gameover:
 
     if len(hand[i])==0:    #if any hand has 0 cards then the game is over
         gameover=True  
-        print ('player won')
+        print ('player ' + str(i+1) + ' won')    #prints after last card in hand is played
     elif len(hand[i])==1:
-        print ('UNO!')
+        print ('UNO!')      #prints after 2nd to last card is played
     if clockwise:
         i=(i+1)%numplayers   #this is how we go from hand[0]and to the other players
                             #and back again to hand [0] 
@@ -282,44 +270,4 @@ while not gameover:
         
 
                   
-            #for card in (hand[i]):
-             #   if canplay(discard[-1],card,wildcolor):
-   # printgamestate(discard,hand,numplayers)
-                
-   # gameover=True
     
-    
-#we are asking hand[0] me/human what card they want to play
-#nested loop that goes with the fuction above to know what is a playable card
-#I did the try block, because at first it said key error
-#it will always be discard[-1]to show what card is in play
-#hand[0]/me,cardtoplay-1, that is what card that I will play
-#I did [cardtoplay-1]because we number the cards1to7 but the program
-    #does 0 to6
-
-#cardtoplay=input('choose a card: ')
-#try:
- #   cardtoplay=int(cardtoplay)
-#except:
- #   print("invalid number")
-#if canplay(discard[-1],hand[0][cardtoplay-1],""):
- #   print("legal")
-#else:
- #   print("illegal")
-    
-#while the game isn't over
- # for each player in the game
-  #  if it is player 0
-   #   print the game state and prompt for a card
-    #  if given a legal card, append it to the discard and remove it from their hand
-     # if it was a black card, prompt for the color choice and set "wildcolor" to it
-    #if it is not player 0
-     # select the first playable card in their hand and play it
-      #if it was a black card
-       # select a random color and print it to the screen, set "wildcolor" to it
-      #print the card played so the human player can see what happened
-    #if the card was a reverse, reverse the direction of the loop
-    #if the card was a skip, move two players forward
-    #if the card wasn't a skip, move one player forward
-    #if the player has 1 card, print "Uno!"
-    #If the player has 0 cards, set a boolean to state the game is over and end the while loop
